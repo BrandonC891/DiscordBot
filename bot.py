9b9205d77e002ec724a66a081e15781dd4dd9798
BrandonC891 <58617118+BrandonC891@users.noreply.github.com>
2020-10-10 03:39:14 -0700

    master

--- bot.py
+++ bot.py
@@ -9,12 +9,16 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+currDir = os.getcwd() 
+confDir = currDir + '/config'
+
 
 setgame_usage = 'Usage: !setgame {game}'
 clear_usage = 'Usage: !clear {size}'
 permission_fail = 'You do not have permission to use this command!'
 hello_response = 'Hello World!'
 update_message = "I'll be back shortly after an update" 
+status_usage = 'Usage: !status {online/idle/dnd/offline} opt:{game}'
 
 
 client = discord.Client()
@@ -26,41 +30,58 @@ def checkAdmin(message):
     return True 
 
 
-def printTime(SERVER, channel, author, raw_message): 
+def printTime(SERVER, channel, author, raw_message, guild): 
     
     print( datetime.datetime.now().strftime("[%Y-%m-%d @ %H:%M:%S]"), end=' ')
-    print( '[' + SERVER + '] '+ '[#'+ str(channel) + '] ' + str(author) + ': ' + raw_message)   
+    print( '[' + SERVER + '] '+ '['+guild.name+'] '+ '[#'+ channel.name + '] ' + author.name + ': ' + raw_message)   
 
     return 
 
 
 @client.event 
 async def on_ready(): 
-    for guild in client.guilds: 
-        if guild.name == GUILD: 
-            break 
     
-    print( 
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
+    #os.environ[TEMP] = "1"
+    #print ( f'Current working directory: ' + currDir + '\n' ) 
+    #print ( f'Config directory: ' + confDir + '\n' )
+
+    #filepath = confDir + '/test.txt'
+    #lines = {} 
+    #with open(filepath, 'r') as file: 
+        #contents = file.readlines() 
+
+    #print(contents)
+
+    #contents[0][0] = 'a' 
+
+    #with open(filepath, 'w') as file: 
+        #file.writelines(contents) 
+
+    #print(contents)
+    
+
+    print( f'{client.user} is connected to the following guild:\n' )
+    for guild in client.guilds: 
+        if guild.name: 
+            print( f'{guild.name}(id: {guild.id})' )
 
 @client.event
 async def on_message(message): 
 
     author = message.author 
     channel = message.channel 
-    raw_message = message.content 
+    raw_message = message.content
+    guild = message.guild  
     #mentions = message.mentions    #unused for now
     #guild = message.guild          #unused for now
     SERVER = "DISCORD"  
 
     if author == client.user: 
         SERVER = "SELFBOT"
-        printTime(SERVER, channel, author, raw_message) 
+        printTime(SERVER, channel, author, raw_message, guild) 
         return 
     
-    printTime(SERVER, channel, author, raw_message)
+    printTime(SERVER, channel, author, raw_message, guild)
 
     #
     # Command : !Hello
@@ -144,5 +165,59 @@ async def on_message(message):
         os.execv(sys.executable, ['python3'] + sys.argv) 
 
 
+    def online(): 
+        return discord.Status.online 
+    def idle(): 
+        return discord.Status.idle 
+    def dnd(): 
+        return discord.Status.dnd
+    def offline(): 
+        return discord.Status.offline
+    
+    options = { 
+            'online' : online, 
+            'idle' : idle, 
+            'dnd' : dnd, 
+            'offline' : offline, 
+    }
+
+    if '!status' in raw_message.lower(): 
+        if raw_message.lower() == '!status': 
+            await channel.send(status_usage)
+            return 
+
+        action = raw_message.split(' ', 2)[1] 
+
+        try: 
+            try: 
+                subaction = raw_message.split(' ', 2)[2]
+                game = discord.Game(subaction) 
+                await client.change_presence(status = options[action](), activity = game)  
+            except: 
+                await client.change_presence(status = options[action]())
+
+            await message.add_reaction('👍') #Confirm command acceptance 
+            print('\t - Updated Status to: ' + action)
+        except: 
+            print('\t - Error: Unknown status')
+            await channel.send('Unknown status: ' + action)
+
+        return 
+
+
+    if raw_message.lower() == '!commands': 
+        filepath = confDir + '/commandList.txt'
+        with open(filepath, 'r') as file: 
+            contents = file.readlines()
+
+        newMsg = ''
+        for i in range(len(contents)): 
+            contents[i] = contents[i].strip('\n')
+            newMsg += contents[i]
+            if i != (len(contents)-1): 
+                newMsg += ', '
+
+        await channel.send('<@{author}> {msg}'.format(author=author.id, msg=newMsg)) 
+        return 
 
 client.run(TOKEN) 
